ffbe5e372842701a72cbe6cdde1fca20f226e4fd
shadyskies <mehta.naman328@gmail.com>
2021-07-08 20:26:13 +0530

    added number of users in room, room leave message

--- django_channels/chat/consumers.py
+++ django_channels/chat/consumers.py
@@ -4,51 +4,8 @@ from asgiref.sync import async_to_sync
 import datetime
 
 
-'''
-#synchronous class implementation
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
-        # get room name from url
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        # create group 
-        self.room_group_name = "chat_%s"%self.room_name
-
-        #join room group (chatconsumer is sync and channel layer is async)
-        async_to_sync(self.channel_layer.group_add)(
-            self.room_group_name,
-            self.channel_name)
-
-        self.accept()
-
-
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
-            self.room_group_name,
-            self.channel_name
-        )
-
-    def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json['message']
-
-        # Send message to room group
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                'type': 'chat_message',
-                'message': message
-            }
-        )
-
-    def chat_message(self, event):
-        message = event['message']
-
-        self.send(text_data=json.dumps({"message":message}))'''
-
-
 # async class implementation
 class ChatConsumer(AsyncWebsocketConsumer):
-
     async def connect(self):
         print(self.scope['user'])
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -68,8 +25,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         print(count)
         time =  datetime.datetime.now().strftime("%H:%M:%S")
 
-    #TODO: implement num of users in room
-       #send room connect message
+        #send room connect message
         await self.channel_layer.group_send(
             self.room_group_name,
             {
@@ -80,6 +36,20 @@ class ChatConsumer(AsyncWebsocketConsumer):
         )
 
     async def disconnect(self, close_code):
+        time =  datetime.datetime.now().strftime("%H:%M:%S")
+        count = getattr(self.channel_layer, self.room_group_name, 0)
+        if not count:
+            setattr(self.channel_layer, self.room_group_name, 1)
+        else:
+            setattr(self.channel_layer, self.room_group_name, count - 1)
+       
+        await self.channel_layer.group_send(
+            self.room_group_name,{
+                'type':'chat_message',
+                'message': f"{self.scope['user']} has left the channel \t\t\t{time}",
+                'count': count
+            }
+        )
         print(f"closed websocket with code: {close_code}")
         # Leave room group
         await self.channel_layer.group_discard(
@@ -94,20 +64,21 @@ class ChatConsumer(AsyncWebsocketConsumer):
        
         # Send message to room group
         time =  datetime.datetime.now().strftime("%H:%M:%S")
-
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
-                'message': message + f"\t\t\t {time}",
+                'message': message + f"\t\t\t {time}"
             }
         )
 
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
-
+        print(event)
+        count = getattr(self.channel_layer, self.room_group_name, 0)
         # Send message to WebSocket
         await self.send(text_data=json.dumps({
             'message': message,
+            'count': count
         }))
